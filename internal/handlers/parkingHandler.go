3fcc905335ddc607379b87676505be97baf6c8c6
Carlos <carlos.2380@hotmail.com>
2024-08-11 14:03:03 +0200

    unused setHeaders

--- internal/handlers/parkingHandler.go
+++ internal/handlers/parkingHandler.go
@@ -1,12 +1,10 @@
 package handlers
 
-import "net/http"
-
 type ParckingHandler struct{}
 
-func setHeaders(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-}
+// func setHeaders(w http.ResponseWriter) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
+// 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+// }
